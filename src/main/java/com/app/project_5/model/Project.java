5a083350c49b27986951093ee798e6bd7b80b5c5
Diana Bagdasaryan <bagdasarandiana953@gmail.com>
2026-09-08 01:27:08 +0700

    пливет

--- src/main/java/com/app/project_5/model/Project.java
+++ src/main/java/com/app/project_5/model/Project.java
@@ -1,4 +1,21 @@
 package com.app.project_5.model;
 
+import java.util.List;
+
 public class Project {
+    // Ошибка рефакторинга: публичные поля (нарушение инкапсуляции)
+    public int id;
+    public String name;
+    public String description;
+    public int priority; // Магическое число
+    public List<Task> tasks;
+
+    // Ошибка рефакторинга: длинный список параметров в конструкторе
+    public Project(int id, String name, String description, int priority, List<Task> tasks) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.priority = priority;
+        this.tasks = tasks;
+    }
 }
