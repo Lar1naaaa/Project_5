775b76b4bd11f6ea66f90ab10edada47446cbdb9
Diana Bagdasaryan <bagdasarandiana953@gmail.com>
2026-09-08 01:41:44 +0700

    теперь по структуре или нет нахрен???😡 хахаха смайлик

--- src/main/java/com/app/project_5/model/Task.java
+++ src/main/java/com/app/project_5/model/Task.java
@@ -1,15 +1,14 @@
 package com.app.project_5.model;
 
 public class Task {
-    // Ошибка рефакторинга: публичные поля вместо инкапсуляции
+    // Публичные поля класса
     public int id;
     public int projectId;
     public String title;
     public String description;
-    public int status; // Магическое число (например, 1 - новая, 2 - в работе...)
-    public int priority; // Магическое число (1-5)
+    public int status;
+    public int priority;
 
-    // Ошибка рефакторинга: длинный список параметров в конструкторе (6 параметров)
     public Task(int id, int projectId, String title, String description, int status, int priority) {
         this.id = id;
         this.projectId = projectId;
