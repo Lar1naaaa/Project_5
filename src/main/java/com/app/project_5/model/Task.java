da11d8895b726052ac5be490330b12966cef73b1
Constantine Mamyshev <proto-types@mail.ru>
2026-09-08 14:32:01 +0700

    Удалил ненужные классы

--- src/main/java/com/app/project_5/model/Task.java
+++ src/main/java/com/app/project_5/model/Task.java
@@ -1,5 +1,7 @@
 package com.app.project_5.model;
 
+import java.util.List;
+
 public class Task {
     // Публичные поля класса
     public int id;
@@ -16,5 +18,23 @@ public class Task {
         this.description = description;
         this.status = status;
         this.priority = priority;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public boolean isCompleted() {
+
+    }
+
+    public long getDurationMinutes() {
+    }
+
+    public <E> List<E> idProperty() {
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.app.project_5 {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens com.app.project_5 to javafx.fxml;
     exports com.app.project_5;
